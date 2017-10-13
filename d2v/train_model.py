e4876a8416bf08505ccd1ea59a4124a79571e8a7
dongyuanxin <yuanxin.me@gmail.com>
2017-10-13 20:20:34 +0800

    doc2vec examples

---
+++ d2v/train_model.py
@@ -0,0 +1,53 @@
+import os,re
+import jieba
+import gensim.models.doc2vec as d2v
+
+try :
+    from .package.clean_html import cleanHtml
+    from .package.cht_or_chs import cht_to_chs
+except:
+    from package.clean_html import cleanHtml
+    from package.cht_or_chs import cht_to_chs
+
+ROOT = os.path.join('..','d2v-data')
+MODEL_PATH = os.path.join('model','news_in_d2v.model')
+
+
+def check(dir=None):
+    def _check(func):
+        def __check(*args,**kwargs):
+            is_raise = (not os.path.isdir(dir)) or (not os.path.exists(dir))
+            if not os.path.isdir(dir):
+                raise Exception("A non-directionary OR not exists")
+            print("over")
+            return func(*args,**kwargs)
+        return __check
+    return _check
+
+@check(ROOT)
+def yieldDocument(root):
+    for nodir in os.listdir(root):
+        _,mode = os.path.splitext(nodir)
+        if mode!='.txt':
+            continue
+
+        filename = os.path.join(root,nodir)
+        with open(filename,'r',encoding='utf-8',errors='ignore') as fin:
+            news_content = fin.read()
+        try:
+            clean_news = cht_to_chs(cleanHtml(news_content))
+        except Exception as e:
+            continue
+        else:
+            rule = re.compile(r'[ \t\n，。！？：“”‘’【】（）\(\)\\…,.!?:"[\]]')
+            clean_news = rule.sub('',clean_news)
+            clean_news = ' '.join(jieba.cut(clean_news))
+        yield d2v.TaggedDocument(clean_news,tags=nodir)
+
+if __name__ == '__main__':
+    documents = yieldDocument(ROOT)
+
+    model = d2v.Doc2Vec(documents=documents,size=1000,min_count=3,workers=32)
+    model.save(MODEL_PATH)
+
+    print("训练完成")
